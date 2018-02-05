a73265eeeec6376c8d33fb474726e93b0f2e94e7
mickwald <mickwald@hotmail.com>
2018-02-05 15:41:05 +0100

    Cleaned up code

--- lab3/lab3-1.c
+++ lab3/lab3-1.c
@@ -50,11 +50,9 @@ GLfloat projectionMatrix[] = {
 };
 
 
-mat4 cameraMatrix;
-
 
 mat4 transform, rot, trans, total, complete, wmWallTransform, wmRoofTransform, wmBalconyTransform;
-mat4 wmBladeTransform, transBlade, rotBlade, groundBoxTransform;
+mat4 wmBladeTransform, transBlade, rotBlade, groundBoxTransform, scale, cameraMatrix;
 //Maek teh madelz!
 Model *wmWalls;
 Model *wmRoof;
@@ -77,81 +75,11 @@ GLuint noshadeprogram;
 unsigned int skyboxTex;
 unsigned int groundTex;
 
-void init(void)
-{
-	// Load windmill
-
-	LoadTGATextureSimple("SkyBox512.tga", &skyboxTex);
-	LoadTGATextureSimple("Grass_tile_B_diffuse.tga", &groundTex);
-	
-	transform = IdentityMatrix();
-	//Load windmill Models
-	wmWalls = LoadModelPlus("windmill/windmill-walls.obj");
-	wmRoof = LoadModelPlus("windmill/windmill-roof.obj");
-	wmBalcony = LoadModelPlus("windmill/windmill-balcony.obj");
-	wmBlade = LoadModelPlus("windmill/blade.obj");
-	groundBox = LoadModelPlus("groundbox.obj");
-	skyBox = LoadModelPlus("skybox.obj");
-	dumpInfo();
-
-
-	cameraMatrix = IdentityMatrix();
-	wmRoofTransform = IdentityMatrix();
-	wmWallTransform = IdentityMatrix();
-	wmBalconyTransform = IdentityMatrix();
-	wmBladeTransform = IdentityMatrix();
-	groundBoxTransform = IdentityMatrix();
-	/*
-	int i;
-	for(i = 0; i<(sizeof(ground)/sizeof(ground[0])); i++){
-		ground[i] *= (float) groundScale;
-	}
-*/
-	// GL inits
-	glClearColor(0.3,0.3,0.3,0);
-	//glDisable(GL_DEPTH_TEST);
-	printError("GL inits");
-	
-	//Projection
-	p = SetVector(0.0f,10.0f,20.0f);
-	l = SetVector(0,10.0f,0);
-	v = SetVector(0,1,0);
-	cameraMatrix = lookAtv(p,l,v);
-	
-	// Load and compile shader
-	program = loadShaders("lab3-1.vert", "lab3-1.frag");
-	noshadeprogram = loadShaders("noshade.vert", "noshade.frag");
-	printError("init shader");
-	
+void drawSkybox(){
 	glUseProgram(noshadeprogram);
-	glUniformMatrix4fv(glGetUniformLocation(noshadeprogram, "uniProjection"), 1, GL_TRUE, projectionMatrix);
-	glUseProgram(program);
-	glUniformMatrix4fv(glGetUniformLocation(program, "uniProjection"), 1, GL_TRUE, projectionMatrix);
-	
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, groundTex);
-	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, skyboxTex);
-	printError("init arrays");
-
-	//Ground
-	
-
-
-	
-
-}
-mat4 scale;
-
-void display(void)
-{
-	printError("pre display");
-
-	// clear the screen
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	//Skybox
+	glDisable(GL_DEPTH_TEST);
 	glDisable(GL_CULL_FACE);
+
 	mat4 skyBoxTransform = cameraMatrix;
 	skyBoxTransform.m[3] = 0;
 	skyBoxTransform.m[7] = 0;
@@ -161,20 +89,22 @@ void display(void)
 	total = Mult(rot, trans);
 	scale = S(1.0f,2.0f,1.0f);
 	total = Mult(total,scale);
+	complete = Mult(skyBoxTransform, total);
 	
 	
 	glUniform1i(glGetUniformLocation(noshadeprogram,"texUnit"),1);
-	complete = Mult(skyBoxTransform, total);
+	glUniform1i(glGetUniformLocation(noshadeprogram,"uniWrap"),1);	
 	glUniformMatrix4fv(glGetUniformLocation(noshadeprogram, "uniTotal"), 1, GL_TRUE, complete.m);
-	glUniform1i(glGetUniformLocation(noshadeprogram,"uniWrap"),1);
-	glUseProgram(noshadeprogram);
-	glDisable(GL_DEPTH_TEST);
-	glDisable(GL_CULL_FACE);
+	
 	DrawModel(skyBox, noshadeprogram, "in_Position", NULL, "inTexCoord");
+	
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
+}
 
+void drawWindmill(){
 	glUseProgram(program);
+	
 	//Windmill transforms	
 	trans = T(0.0f, 0, 0.0f);
 	rot = Ry(0);
@@ -210,66 +140,157 @@ void display(void)
 		glUniformMatrix4fv(glGetUniformLocation(program, "uniTotal"), 1, GL_TRUE, complete.m);
 		DrawModel(wmBlade, program, "in_Position", "in_Normal", "inTexCoord");
 	}
+}
+
+void drawGround(){
 	glUseProgram(noshadeprogram);
-	//Ground
 	trans = T(0.0f, -1.0f, 0.0f);
 	rot = Ry(0);
 	total = Mult(rot, trans);
 	scale = S(100.0f,0.5f,100.0f);
 	total = Mult(total,scale);
+	complete = Mult(cameraMatrix,Mult(total,groundBoxTransform));
 
 	glUniform1i(glGetUniformLocation(noshadeprogram,"uniWrap"),100);
 	glUniform1i(glGetUniformLocation(noshadeprogram,"texUnit"),0);
-	complete = Mult(cameraMatrix,Mult(total,groundBoxTransform));
 	glUniformMatrix4fv(glGetUniformLocation(noshadeprogram, "uniTotal"), 1, GL_TRUE, complete.m);
-	glUseProgram(noshadeprogram);
 	DrawModel(groundBox, noshadeprogram, "in_Position", NULL, "inTexCoord");
+}
+
+void CheckInput(){
+	if(glutKeyIsDown('w')){
+		cameraMatrix = Mult(cameraMatrix, T(0,0,0.2f));
+	}	
+	if(glutKeyIsDown('s')){
+		cameraMatrix = Mult(cameraMatrix, T(0,0,-0.2f));
+	}	
+	if(glutKeyIsDown('a')){
+		cameraMatrix = Mult(cameraMatrix, T(0.2f,0,0));
+	}	
+	if(glutKeyIsDown('d')){
+		cameraMatrix = Mult(cameraMatrix, T(-0.2f,0,0));
+	}	
+	if(glutKeyIsDown('i')){
+		cameraMatrix = Mult(cameraMatrix, T(0,-0.2f,0));
+	}	
+	if(glutKeyIsDown('k')){
+		cameraMatrix = Mult(cameraMatrix, T(0,0.2f,0));
+	}
+	if(glutKeyIsDown('j')){
+		cameraMatrix = Mult(Ry(-1*M_PI/180),cameraMatrix );
+	}
+	if(glutKeyIsDown('l')){
+		cameraMatrix = Mult(Ry(1*M_PI/180),cameraMatrix );
+	}	
+}
+
+void loadModels(){
+	wmWalls = LoadModelPlus("windmill/windmill-walls.obj");
+	wmRoof = LoadModelPlus("windmill/windmill-roof.obj");
+	wmBalcony = LoadModelPlus("windmill/windmill-balcony.obj");
+	wmBlade = LoadModelPlus("windmill/blade.obj");
+	groundBox = LoadModelPlus("groundbox.obj");
+	skyBox = LoadModelPlus("skybox.obj");
+}
+
+void initTransforms(){
+	cameraMatrix = wmRoofTransform = wmWallTransform = wmBalconyTransform = wmBladeTransform = groundBoxTransform = transform = IdentityMatrix();
+}
+
+
+void uploadProjectionMatrices(){
+	glUseProgram(noshadeprogram);
+	glUniformMatrix4fv(glGetUniformLocation(noshadeprogram, "uniProjection"), 1, GL_TRUE, projectionMatrix);
+	glUseProgram(program);
+	glUniformMatrix4fv(glGetUniformLocation(program, "uniProjection"), 1, GL_TRUE, projectionMatrix);
+}
+
+
+void uploadTextures(){
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, groundTex);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, skyboxTex);
+}
+
+void initShaders(){
+	program = loadShaders("lab3-1.vert", "lab3-1.frag");
+	noshadeprogram = loadShaders("noshade.vert", "noshade.frag");
+}
+
+
+//Init here
+void init(void)
+{
+	// Load windmill
+
+	LoadTGATextureSimple("SkyBox512.tga", &skyboxTex);
+	LoadTGATextureSimple("Grass_tile_B_diffuse.tga", &groundTex);
+	
+	//Load Models
+	loadModels();
+	dumpInfo();
+	
+	initTransforms();
+
+	// GL inits
+	glClearColor(0.3,0.3,0.3,0);
+	printError("GL inits");
+	
+	//Projection
+	p = SetVector(0.0f,10.0f,20.0f);
+	l = SetVector(0,10.0f,0);
+	v = SetVector(0,1,0);
+	cameraMatrix = lookAtv(p,l,v);
+	
+	// Load and compile shader
+	initShaders();
+	printError("init shader");
+	
+	uploadProjectionMatrices();
+	
+	uploadTextures();
+
+	printError("init arrays");
+
+	//Ground
+	
+
+
+	
+
+}
+
+
+//Draw call
+void display(void)
+{
+	printError("pre display");
+
+	// clear the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	//Skybox
+	drawSkybox();
+
+	//Windmill
+	drawWindmill();
+	
+	//Ground
+	drawGround();
 
 	printError("display");
 	
 	glutSwapBuffers();
 }
-//uniProjection * uniCamera * uniTotal * uniTransform
+
 
 void onTimer(int value)
 {
 	
-	t = (GLfloat)glutGet(GLUT_ELAPSED_TIME) * M_PI / (2*1000);
+	t = (GLfloat) glutGet(GLUT_ELAPSED_TIME) * M_PI / (2*1000);
 	//Camera Controls
-	if(glutKeyIsDown('w')){
-		//p = SetVector(p.x, p.y+0.2f, p.z);
-		cameraMatrix = Mult(cameraMatrix, T(0,0,0.2f));
-	}	
-	if(glutKeyIsDown('s')){
-		//p = SetVector(p.x, p.y-0.2f, p.z);
-		cameraMatrix = Mult(cameraMatrix, T(0,0,-0.2f));
-	}	
-	if(glutKeyIsDown('a')){
-		//p = SetVector(p.x-0.2f, p.y, p.z);
-		cameraMatrix = Mult(cameraMatrix, T(0.2f,0,0));
-	}	
-	if(glutKeyIsDown('d')){
-		//p = SetVector(p.x+0.2f, p.y, p.z);
-		cameraMatrix = Mult(cameraMatrix, T(-0.2f,0,0));
-	}	
-	if(glutKeyIsDown('i')){
-		//p = SetVector(p.x, p.y, p.z-0.2f);
-		cameraMatrix = Mult(cameraMatrix, T(0,-0.2f,0));
-	}	
-	if(glutKeyIsDown('k')){
-		//p = SetVector(p.x, p.y, p.z+0.2f);
-		cameraMatrix = Mult(cameraMatrix, T(0,0.2f,0));
-	}
-	if(glutKeyIsDown('j')){
-		//p = SetVector(p.x, p.y, p.z+0.2f);
-		cameraMatrix = Mult(Ry(-1*M_PI/180),cameraMatrix );
-	}
-	if(glutKeyIsDown('l')){
-		//p = SetVector(p.x, p.y, p.z+0.2f);
-		cameraMatrix = Mult(Ry(1*M_PI/180),cameraMatrix );
-	}	
-	
-
+	CheckInput();
 
 	glUniform1f(glGetUniformLocation(program, "uniTime"), t*10);
 	glutPostRedisplay();	
@@ -289,3 +310,6 @@ int main(int argc, char *argv[])
 	glutMainLoop();
 	return 0;
 }
+
+
+
